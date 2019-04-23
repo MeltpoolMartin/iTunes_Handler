0404b51b3b43acdd18779f5b5a2467b398371d3c
Martin Wachter <martinwachter@gmx.net>
2019-04-23 20:30:15 +0200

    uploading iTunes project in Github

---
+++ iTunes_error/confirm_error_message.py
@@ -0,0 +1,19 @@
+import pyautogui as gui
+
+
+#gui.press("enter")
+
+# size = gui.size()
+# print(size)
+# x_middle = size[0]//2
+# y_middle = size[1]//2
+
+#print(f"X = {x_middle} Y = {y_middle}")
+#gui.rightClick(x_middle, y_middle)
+
+
+# locate_pos = gui.locateOnScreen('Screenshots/finder_logo.png')
+# if locate_pos is None:
+#     print("Not found")
+# else:
+#     print(locate_pos)
