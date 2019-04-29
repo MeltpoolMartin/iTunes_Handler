ca9bd16dde341ab476d03633e7517b01fd96e91c
Martin Wachter <martinwachter@gmx.net>
2019-04-29 23:11:01 +0200

    Adding logging

---
+++ iTunes_error/open_file/open_file.py
@@ -0,0 +1,16 @@
+
+class Open_File():
+    '''
+           opens a file of a given filename, returns the file reference
+           and closes the reference
+       '''
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
