411823684eb5c1b7f149e13e664d2f54aeb8f2fe
Martin Wachter <martinwachter@gmx.net>
2019-05-01 18:06:56 +0200

    Adding logging feature

--- iTunes_error/confirm_error_message.py
+++ iTunes_error/confirm_error_message.py
@@ -1,19 +1,51 @@
+import sys_proc.get_proc_attrs as get_proc_attrs
 import sys_proc.check_if_proc_is_running as check_if_proc_is_running
+import logging
 import time
+import pyautogui as gui
 
 
 if __name__ == '__main__':
 
+    #Initialize Logger
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    # create a file handler
+    handler = logging.FileHandler(f'{__name__}.log')
+    handler.setLevel(logging.INFO)
+
+    # create a logging format
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(handler)
+    # yyyy-MM-dd HH:mm:ss,SSS
+
+
     #define inital conditions
     proc_name = 'excel'
     proc_found = 'false'
+    counter = 0
+    logger.info("Initialization finished")
 
-    #wait unitl proc_name was started
+    #wait unitl proc_name was started or timeout
     while proc_found != True:
-        time.sleep(1) #wait for 1 second until checking for process
+        proc_info = get_proc_attrs.get_proc_attrs(proc_name)
         proc_found = check_if_proc_is_running.check_if_proc_is_running(proc_name)
-    print(f'{proc_name} was started')
-
+        if proc_found == True:
+            logger.info(f"{proc_name} was started")
+            logger.info('%s', proc_info)
+            time.sleep(5)
+            gui.press('enter')
+            logger.info("Enter key was pressed")
+        time.sleep(1)  # wait for 1 second until checking for process
+        counter += 1
+        if counter == 10:
+            logger.warning('Timeout after 10 sec')
+            break
 else:
     print(f'{__name__} is not run as main')
 
--- iTunes_error/test.py
+++ iTunes_error/test.py
@@ -1,6 +1,32 @@
-import sys_proc.check_if_proc_is_running
-import sys_proc.get_proc_attrs
+import subprocess
+import time
+import sys_proc.get_proc_attrs as get_proc_attr
+import logging
 
-name = "excel"
-print(sys_proc.check_if_proc_is_running.check_if_proc_is_running(name))
-print(sys_proc.get_proc_attrs.get_proc_attrs(name))
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# create a file handler
+handler = logging.FileHandler(f'{__name__}.log')
+handler.setLevel(logging.INFO)
+
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
+#yyyy-MM-dd HH:mm:ss,SSS
+
+i = 0
+logger.info("Initialization finished")
+
+"""subprocess.call("open -a TextEdit")
+logging.info("Program started via subprocess call")
+
+while i < 5:
+    print(i)
+    time.sleep(1)
+    i += 1
+proc_info = get_proc_attr.get_proc_attrs("TexEdit")
+"""
