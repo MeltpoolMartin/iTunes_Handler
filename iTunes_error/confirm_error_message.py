2b9c5580dc863414b16772dbe97817c77ee9c27f
Martin Wachter <martinwachter@gmx.net>
2019-08-04 22:07:19 +0200

    Adding iTunes_error_cfg.json file to save previous music library conditions; reading json file and compare it with current music library conditions

--- iTunes_error/confirm_error_message.py
+++ iTunes_error/confirm_error_message.py
@@ -1,6 +1,7 @@
 import sys_proc.get_proc_attrs as get_proc_attrs
 import sys_proc.check_if_proc_is_running as check_if_proc_is_running
 import logging
+import json
 import logging.handlers
 import subprocess
 import os
@@ -9,16 +10,80 @@ import pyautogui as gui
 from enum import Enum
 
 
+def write_cfg(lib_data={}):
+    with open('iTunes_error_cfg/iTunes_error_cfg.json', 'w') as cfg:
+        json.dump(lib_data, cfg)
+
+
+def read_cfg(path):
+    """
+    Reads in a json file from the given path and returns a dictionary
+    @param path: path of the json file
+    @return: content of the json file as dictionary
+    """
+    if os.path.splitext(path)[-1] == '.json':
+        with open(path, 'r') as cfg:
+            return json.load(cfg)
+    else:
+        return None
+
+
+def count_files(path):
+    """
+    count_files considers every file in the given path and counts it to a dict
+    @param path:path, which will be analyzed for changes/modifications
+    @return:dictionary with the count result
+    """
+
+    if os.path.isdir(path) is True:
+        lib_data = {}
+        for folder_name, sub_folders, file_names in os.walk(path):
+            for file_name in file_names:
+                name, extension = os.path.splitext(os.path.join(path, folder_name,  file_name))
+                #sort out ".DS_Store
+                if os.path.split(name)[-1] != ".DS_Store":
+                    lib_data.setdefault(extension.lower(), 0)
+                    lib_data[extension.lower()] += 1
+        return lib_data
+    else:
+        print('Given path should only be a directory')
+
+
+def check_4_lib_update(json_path, lib_path):
+    """
+    Compares the counted extension from the json script and the current music library, if any files were added or removed
+    @param json_path: path of the json file
+    @param lib_path:  path of the music library
+    @return: False --> no update required; True --> update required
+    """
+    if (os.path.splitext(json_path)[-1] == '.json') and (os.path.isdir(lib_path) is True):
+        new_lib_data = count_files(lib_path)
+        old_lib_data = read_cfg(json_path)
+        for key in new_lib_data.keys():
+            if key in old_lib_data.keys():
+                if new_lib_data[key] != old_lib_data[key]:
+                    print(f'{key} new:{new_lib_data[key]} old: {old_lib_data[key]}')
+                    return True
+            else:
+                print(f'missing in old keys: {key}')
+                return True
+        return False
+    else:
+        return None
+
+
 class State(Enum):
 
     INIT = 1
     LAUNCH_PROCESS = 2
     FIND_PROCESS = 3
     CHECK_TIME = 4
-    UPDATE_LIBRARY = 5
-    KILL_PROCESS = 6
-    SHUTDOWN = 7
-    STOP = 8
+    CHECK_4_UPDATE = 5
+    UPDATE_LIBRARY = 6
+    KILL_PROCESS = 7
+    SHUTDOWN = 8
+    STOP = 9
+
 
 if __name__ == '__main__':
 
@@ -39,12 +104,16 @@ if __name__ == '__main__':
             #Initialize LAUNCH PROCESS
             #app_name = 'calc.exe'
             app_name = 'iTunes'
+            app_path = 'C:\Program Files\iTunes\iTunes.exe'
+            lib_path = '/Volumes/music'
+            json_path = 'iTunes_error_cfg/iTunes_error_cfg.json'
 
             #define inital conditions
             hh, mm, ss = 23, 55, 0 # timestamp to initialize termination of the application
             proc_name = app_name #'iTunes'  # name of the application to search for
             proc_found = 'false'
             counter = 0
+
             logger.info("%s - Initialization finished", State)
             logger.info(f"{State} - App Name = {app_name}")
             logger.info(f"{State} - Termination Time = {hh}:{mm}:{ss}")
@@ -57,7 +126,7 @@ if __name__ == '__main__':
                 #                  stdout=subprocess.PIPE)  # open /Applications/Calculator.app
                 #proc = subprocess.Popen([app_name], cwd="C:\Program Files\iTunes",
                 #                                   stdout=subprocess.PIPE)
-                proc = subprocess.Popen(["C:\Program Files\iTunes\iTunes.exe"], stdout=subprocess.PIPE)
+                proc = subprocess.Popen([app_path], stdout=subprocess.PIPE)
                 logger.info(f"{State} - {proc_name} was started")
             except ValueError as err:
                 logger.info(f"{State} - {proc_name} could not be started")
@@ -77,7 +146,7 @@ if __name__ == '__main__':
                 time.sleep(5)
                 gui.press('enter') # enter key confirms the error message
                 logger.info("%s - Enter key was pressed", State)
-                State = State.UPDATE_LIBRARY
+                State = State.CHECK_4_UPDATE
 
             time.sleep(1)  # wait for 1 second until checking for process
             counter += 1
@@ -96,6 +165,12 @@ if __name__ == '__main__':
                     logger.info(f'{State} - Shutdown time reached')
                     State = State.KILL_PROCESS
                     break
+
+        if State == State.CHECK_4_UPDATE:
+            if check_4_lib_update(json_path, lib_path) is True:
+                State = State.UPDATE_LIBRARY
+            else:
+                State = State.CHECK_TIME
 
         if State == State.UPDATE_LIBRARY:
             logger.info(f"{State} - Updating iTunes library")
--- iTunes_error/test.py
+++ iTunes_error/test.py
@@ -1,6 +1,74 @@
-import time
+import json
+import os
 
-while True:
-    hh, mm, ss = time.localtime()[3:6]
-    print('\r', f'{hh}:{mm}:{ss}', end="")
-    time.sleep(1)
+
+def write_cfg(lib_data={}):
+    with open('iTunes_error_cfg/iTunes_error_cfg.json', 'w') as cfg:
+        json.dump(lib_data, cfg)
+
+
+def read_cfg(path):
+    """
+    Reads in a json file from the given path and returns a dictionary
+    @param path: path of the json file
+    @return: content of the json file as dictionary
+    """
+    if os.path.splitext(path)[-1] == '.json':
+        with open(path, 'r') as cfg:
+            return json.load(cfg)
+    else:
+        return None
+
+
+def count_files(path):
+    """
+    count_files considers every file in the given path and counts it to a dict
+    @param path:path, which will be analyzed for changes/modifications
+    @return:dictionary with the count result
+    """
+
+    if os.path.isdir(path) is True:
+        lib_data = {}
+        for folder_name, sub_folders, file_names in os.walk(path):
+            for file_name in file_names:
+                name, extension = os.path.splitext(os.path.join(path, folder_name,  file_name))
+                #sort out ".DS_Store
+                if os.path.split(name)[-1] != ".DS_Store":
+                    lib_data.setdefault(extension.lower(), 0)
+                    lib_data[extension.lower()] += 1
+        return lib_data
+    else:
+        print('Given path should only be a directory')
+
+
+def check_4_lib_update(json_path, lib_path):
+    """
+    Compares the counted extension from the json script and the current music library, if any files were added or removed
+    @param json_path: path of the json file
+    @param lib_path:  path of the music library
+    @return: False --> no update required; True --> update required
+    """
+    if (os.path.splitext(json_path)[-1] == '.json') and (os.path.isdir(lib_path) is True):
+        new_lib_data = count_files(lib_path)
+        old_lib_data = read_cfg(json_path)
+        for key in new_lib_data.keys():
+            if key in old_lib_data.keys():
+                if new_lib_data[key] != old_lib_data[key]:
+                    print(f'{key} new:{new_lib_data[key]} old: {old_lib_data[key]}')
+                    return True
+            else:
+                print(f'missing in old keys: {key}')
+                return True
+        return False
+    else:
+        return None
+
+
+
+
+
+data = {'.plist': 1, '.jpg': 1072, '.ini': 176, '.mp3': 5991, '.db': 58, '.nfo': 7, '.m4a': 4, '.rar': 2, '.r00': 1, '.tmp': 1, '.txt': 4, '.png': 1, '.pdf': 2, '.p4u': 1, '.cld': 2, '.m3u': 37, '.sfv': 6, '.url': 4, '.jpeg': 1, '.zip': 1, '.strings': 39}
+
+#write_cfg(data)
+#data = read_cfg('/Users/Martin/GitKraken/iTunes_error/iTunes_error/')
+print(check_4_lib_update(json_path='iTunes_error_cfg/iTunes_error_cfg.json', lib_path='/Volumes/music'))
